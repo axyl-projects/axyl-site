8674bd3a111e8ca6210601a66dfcb9cb3faf6971
Austin 'AWFixer' Ware <awfixer@pm.me>
2024-11-05 04:09:37 -0500

    update domain and docs config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://axyl-projects.github.io/',
-  base: '/axyl-site',
+  site: 'https://axyl.org/',
+  base: '/',
   integrations: [
     starlight({
-      title: 'Athena OS',
-      description: 'Athena OS, a new pentesting/cybersecurity linux distribution.',
+      title: 'AxylOS',
+      description: 'AxylOS, a new FLOSS Distrobution, based on the helctic kernel.',
       components: {
         TableOfContents: "./src/components/TOC.astro",
         PageTitle: "./src/components/CustomPageTitle.astro"
@@ -20,85 +20,85 @@ export default defineConfig({
       },
       customCss: process.env.NO_GRADIENTS ? [	'./src/styles/_global.css'] : ['./src/styles/landing.css', 	'./src/styles/_global.css'],
       social: {
-        github: 'https://github.com/Athena-OS',
-        instagram: 'https://www.instagram.com/athenaos_sec',
-        discord: 'https://discord.gg/athena-os-977645785170714644',
+        github: 'https://github.com/Axyl-Projects',
+        twitter: 'https://x.com/@axylprojects',
+        discord: 'https://discord.gg/J7R7sEZcn9',
       },
       editLink: {
-        baseUrl: 'https://github.com/Athena-OS/athena-docs/tree/main/',
+        baseUrl: 'https://github.com/Axyl-projects/axyl-site/tree/main/',
       },
       lastUpdated: true,
-      sidebar: [
-        {
-          label: 'Getting Started',
-          items: [
+      //sidebar: [
+        //{
+        //  label: 'Getting Started',
+        //  items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Manifesto', link: '/en/getting-started/manifesto' },
-            { label: 'Why Athena OS?', link: '/en/getting-started/athenaos' },
-            { label: 'Downloading Athena OS', link: '/en/getting-started/download' },
-          ],
-        },
-        {
-          label: 'Installation',
-          items: [
+        //    { label: 'Manifesto', link: '/en/getting-started/manifesto' },
+        //    { label: 'Why Athena OS?', link: '/en/getting-started/athenaos' },
+        //    { label: 'Downloading Athena OS', link: '/en/getting-started/download' },
+        //  ],
+        //},
+        //{
+        //  label: 'Installation',
+        //  items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Installation prepare', link: '/en/installation/prepare-install' },
-            { label: 'Installing as single boot', link: '/en/installation/install-single-boot' },
-            { label: 'Installing as dual boot with Linux or Windows', link: '/en/installation/install-dual-boot' },
-            { label: 'Installing by NixOS', link: '/en/installation/install-nixos' },
-            { label: 'Installing as persistent USB', link: '/en/installation/install-persistent-usb' },
-            { label: 'Installing as guest in VMware', link: '/en/installation/install-guest-vmware' },
-            { label: 'Installing as guest in VirtualBox', link: '/en/installation/install-guest-virtualbox' },
-            { label: 'Installing as guest in Hyper-V', link: '/en/installation/install-guest-hyperv' },
-            { label: 'Installing as guest in QEMU/KVM', link: '/en/installation/install-guest-qemu' },
-            { label: 'Installing as guest in Quickemu', link: '/en/installation/install-guest-quickemu' },
-            { label: 'Installing as guest in Parallels', link: '/en/installation/install-guest-parallels' },
-            { label: 'Installing as guest in UTM', link: '/en/installation/install-guest-utm' },
-            { label: 'Updating Athena OS', link: '/en/installation/update-athena' },
-            { label: 'Aegis Installer TUI', link: '/en/installation/aegis-tui' },
-          ],
-        },
-        {
-          label: 'Configuration',
-          items: [
+        //   { label: 'Installation prepare', link: '/en/installation/prepare-install' },
+        //    { label: 'Installing as single boot', link: '/en/installation/install-single-boot' },
+        //    { label: 'Installing as dual boot with Linux or Windows', link: '/en/installation/install-dual-boot' },
+        //    { label: 'Installing by NixOS', link: '/en/installation/install-nixos' },
+        //    { label: 'Installing as persistent USB', link: '/en/installation/install-persistent-usb' },
+        //    { label: 'Installing as guest in VMware', link: '/en/installation/install-guest-vmware' },
+        //    { label: 'Installing as guest in VirtualBox', link: '/en/installation/install-guest-virtualbox' },
+        //    { label: 'Installing as guest in Hyper-V', link: '/en/installation/install-guest-hyperv' },
+        //    { label: 'Installing as guest in QEMU/KVM', link: '/en/installation/install-guest-qemu' },
+        //    { label: 'Installing as guest in Quickemu', link: '/en/installation/install-guest-quickemu' },
+        //    { label: 'Installing as guest in Parallels', link: '/en/installation/install-guest-parallels' },
+        //    { label: 'Installing as guest in UTM', link: '/en/installation/install-guest-utm' },
+        //    { label: 'Updating Athena OS', link: '/en/installation/update-athena' },
+        //    { label: 'Aegis Installer TUI', link: '/en/installation/aegis-tui' },
+        //  ],
+        //},
+        //{
+        //  label: 'Configuration',
+        //  items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Athena Nix', link: '/en/configuration/nix-config' },
+        //    { label: 'Athena Nix', link: '/en/configuration/nix-config' },
             // { label: 'Calamares Installer', link: '/en/configuration/calamares' },
-            { label: 'Kernel', link: '/en/configuration/kernel' },
-            { label: 'Shell', link: '/en/configuration/shell' },
-            { label: 'Display Manager', link: '/en/configuration/display-manager' },
-            { label: 'GNOME', link: '/en/configuration/gnome' },
-            { label: 'Bspwm', link: '/en/configuration/bspwm' },
-            { label: 'KDE Plasma', link: '/en/configuration/kde' },
-            { label: 'MATE', link: '/en/configuration/mate' },
-            { label: 'Cinnamon', link: '/en/configuration/cinnamon' },
-            { label: 'XFCE', link: '/en/configuration/xfce' },
-            { label: 'Hyprland', link: '/en/configuration/hyprland' },
-            { label: 'NVIDIA GPU', link: '/en/configuration/nvidia' },
-            { label: 'RDP', link: '/en/configuration/rdp' },
-            { label: 'Repositories', link: '/en/configuration/repositories' },
-            { label: 'Themes', link: '/en/configuration/themes' },
-            { label: 'Utilities', link: '/en/configuration/utilities' },
-          ],
-        },
-        {
-          label: 'Cyber Resources',
-          items: [
+        //    { label: 'Kernel', link: '/en/configuration/kernel' },
+        //    { label: 'Shell', link: '/en/configuration/shell' },
+        //    { label: 'Display Manager', link: '/en/configuration/display-manager' },
+        //    { label: 'GNOME', link: '/en/configuration/gnome' },
+        //    { label: 'Bspwm', link: '/en/configuration/bspwm' },
+        //    { label: 'KDE Plasma', link: '/en/configuration/kde' },
+        //    { label: 'MATE', link: '/en/configuration/mate' },
+        //    { label: 'Cinnamon', link: '/en/configuration/cinnamon' },
+        //    { label: 'XFCE', link: '/en/configuration/xfce' },
+        //    { label: 'Hyprland', link: '/en/configuration/hyprland' },
+        //    { label: 'NVIDIA GPU', link: '/en/configuration/nvidia' },
+        //    { label: 'RDP', link: '/en/configuration/rdp' },
+        //    { label: 'Repositories', link: '/en/configuration/repositories' },
+        //    { label: 'Themes', link: '/en/configuration/themes' },
+        //    { label: 'Utilities', link: '/en/configuration/utilities' },
+        //  ],
+        //},
+        //{
+        //  label: 'Cyber Resources',
+        //  items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Athena Welcome', link: '/en/resources/athena-welcome' },
-            { label: 'Pentesting Tools', link: '/en/resources/pentesting-tools' },
-            { label: 'Athena Cyber Hub', link: '/en/resources/cyber-hub' },
-            { label: 'Hack The Box Toolkit', link: '/en/resources/htb-toolkit' },
-            { label: 'PWNage Menu', link: '/en/resources/pwnage-menu' },
-            { label: 'Blue Team Menu', link: '/en/resources/blueteam-menu' },
-            { label: 'Red Team Menu', link: '/en/resources/redteam-menu' },
-            { label: 'Payload to Dock', link: '/en/resources/payload-dock' },
-            { label: 'Payload Environment Variables', link: '/en/resources/payload-variables' },
-            { label: 'Browser Pentesting Resources', link: '/en/resources/browser-pentesting' },
-            { label: 'NIST Feed', link: '/en/resources/nist-feed' },
-            { label: 'Devotio', link: '/en/resources/devotio' },
-          ],
-        },
+        //    { label: 'Athena Welcome', link: '/en/resources/athena-welcome' },
+        //    { label: 'Pentesting Tools', link: '/en/resources/pentesting-tools' },
+        //    { label: 'Athena Cyber Hub', link: '/en/resources/cyber-hub' },
+        //    { label: 'Hack The Box Toolkit', link: '/en/resources/htb-toolkit' },
+        //    { label: 'PWNage Menu', link: '/en/resources/pwnage-menu' },
+        //    { label: 'Blue Team Menu', link: '/en/resources/blueteam-menu' },
+        //    { label: 'Red Team Menu', link: '/en/resources/redteam-menu' },
+        //    { label: 'Payload to Dock', link: '/en/resources/payload-dock' },
+        //    { label: 'Payload Environment Variables', link: '/en/resources/payload-variables' },
+        //    { label: 'Browser Pentesting Resources', link: '/en/resources/browser-pentesting' },
+        //    { label: 'NIST Feed', link: '/en/resources/nist-feed' },
+        //    { label: 'Devotio', link: '/en/resources/devotio' },
+        //   ],
+        //},
         //{
         //  label: 'Customization',
         //  items: [
